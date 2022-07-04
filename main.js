a1a3d4eaa590926c9eef5333bd4503c4e516dabe
Mrseptentrio <mrseptenrio@gmail.com>
2022-07-04 22:59:03 +0100

    Finalizing the terminal based project

--- main.js
+++ main.js
@@ -1,22 +1,75 @@
+function game() {
 
+  
 // Here I create a function that holds an array of possble answers
 // I then create a variable that will hold the final choice by randomizing
 // through the array and picking a different answer everytime
-function computerPlay() {
+ function computerPlay()  {
   let choices = ['rock', 'paper', 'scisor'];
-  let choice = choices[Math.floor(Math.random() * choices.length)];
+  let choice = choices[Math.floor(Math.random() * 3)];
+  return choice;
 }
+const playerSelection = prompt('Please make a choice: ').toLowerCase();
+const computerSelection = computerPlay();
 
+
+
+
+console.log("User choice: ", playerSelection);
+console.log("Computer choice: ", computerSelection);
+console.log(playRound(playerSelection, computerSelection))
 
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == 'rock' && computerSelection == 'scisor') {
-    return 'rock wins';
-}
+
+  if (playerSelection === computerSelection) {
+    return 'It\'s a tie';
+  }
+
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'scisor') {
+      return 'GG, rock wins !';
+    } else {
+      return 'You lost, paper wins';
+    }
+  }
+
+  if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') {
+      return 'GG, paper wins !';
+    } else {
+      return 'You lost, scisor wins !'
+    }
+  }
+
+  if (playerSelection === 'scisor') {
+    if (computerSelection === 'paper') {
+      return 'GG, scisor wins !';
+    } else {
+      return 'You lost, rock wins';
+    }
+  }
+
+  if (playerSelection !== 'scisor' || 'rock' || 'paper') {
+    return 'Please chose one of three: rock, paper, scisor'
+  }
 
 };
-//Prompt the user to input a choice
-const playerSelection = prompt('Please make a choice: ').toLowerCase();
+    return playRound(playerSelection, computerSelection)
 
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection))
+
+
+
+
+
+
+
+
+}
+
+for (let i = 0; i <= 5; i++) {
+  game();
+}
+
+
+//Prompt the user to input a choice
