a6b82cd838bcdd83c2ad1dc9cadcc0a9b3adf4fd
Mrseptentrio <mrseptenrio@gmail.com>
2022-07-17 17:12:00 +0100

    adding the ui app

--- main.js
+++ main.js
@@ -1,77 +1,67 @@
-// Initializing the function that runs the entire game
-function game() {
+const warriorChoiceShow = document.querySelector('.warriorChoice')
+const aiChoiceShow = document.querySelector('.aiChoice');
+const ending = document.querySelector('.ending');
+const choices = document.querySelectorAll('button');
+const warriorScoreShow = document.querySelector('.warriorScore');
+const aiScoreShow = document.querySelector('.aiScore');
 
+let warriorScore = 0;
+let aiScore = 0;
+
+let warriorChoice;
+let aiChoice;
+let result;
+let score;
+		
+choices.forEach(choice => choice.addEventListener('click', (e) => {
+  warriorChoice = e.target.id;
+  warriorChoiceShow.innerHTML = warriorChoice;
+  generateComputerChoice();
+  matchResult();
+}))
+
+function generateComputerChoice() {
+  const compRandomChoice = Math.floor(Math.random() * 3) + 1;
+
+  if (compRandomChoice === 1) {
+    aiChoice = 'rock';
+  } else if (compRandomChoice === 2) {
+    aiChoice = 'paper';
+  } else if (compRandomChoice === 3) {
+    aiChoice = 'scisor';
+  } else {
+    aiChoice.innerHTML = 'Woops, something went wrong';
+  }
+  aiChoiceShow.innerHTML = aiChoice;
+
+}
+
+function matchResult() {
+  if (warriorChoice === aiChoice) {
+    result = 'it\'s a tie!';
+  } else if (warriorChoice === 'rock' && aiChoice === 'scisor') {
+    result = 'You win !';
+    warriorScore += 1;
+  } else if (warriorChoice === 'paper' && aiChoice === 'rock') {
+    result = 'You win';
+    warriorScore += 1;
+  } else if (warriorChoice === 'scisor' && aiChoice === 'paper') {
+    result = 'You win';
+    warriorScore += 1;
+  } else if (warriorChoice === 'scisor' && aiChoice === 'rock') {
+    result = 'You lost!';
+    aiScore += 1;
+  } else if (warriorChoice == 'rock' && aiChoice === 'paper') {
+    result = 'You lost!';
+    aiScore += 1;
+  } else if (warriorChoice === 'paper' && aiChoice === 'scisor') {
+    result = 'You lost!';
+    aiScore += 1;
+  }
+  warriorScoreShow.innerHTML = warriorScore;
+  aiScoreShow.innerHTML = aiScore;
+  ending.innerHTML = result;
   
-// Here I create a function that holds an array of possble answers
-// I then create a variable that will hold the final choice by randomizing
-// through the array and picking a different answer everytime
- function computerPlay()  {
-  let choices = ['rock', 'paper', 'scisor'];
-  let choice = choices[Math.floor(Math.random() * 3)];
-  return choice;
+  
 }
-  // prompting the player for an input
-const playerSelection = prompt('Please make a choice: ').toLowerCase();
-  // Assigning the ComputerPlay function to a variable
-const computerSelection = computerPlay();
 
-
-
-// printing the choices and results to the console
-console.log("User choice: ", playerSelection);
-console.log("Computer choice: ", computerSelection);
-console.log(playRound(playerSelection, computerSelection))
-
-// This function takes the two players input, process them through
-  // a series of conditions to decide whos the winner
-  // TODO Rewriting the conditions for better readability and in a more simple/beautiful way
-function playRound(playerSelection, computerSelection) {
-
-  if (playerSelection === computerSelection) {
-    return 'It\'s a tie';
-  }
-
-  if (playerSelection === 'rock') {
-    if (computerSelection === 'scisor') {
-      return 'GG, rock wins !';
-    } else {
-      return 'You lost, paper wins';
-    }
-  }
-
-  if (playerSelection === 'paper') {
-    if (computerSelection === 'rock') {
-      return 'GG, paper wins !';
-    } else {
-      return 'You lost, scisor wins !'
-    }
-  }
-
-  if (playerSelection === 'scisor') {
-    if (computerSelection === 'paper') {
-      return 'GG, scisor wins !';
-    } else {
-      return 'You lost, rock wins';
-    }
-  }
-  // rejecting all other choices
-  if (playerSelection !== 'scisor' || 'rock' || 'paper') {
-    return 'Please chose one of three: rock, paper, scisor'
-  }
-
-};
-    return playRound(playerSelection, computerSelection)
-
-
-
-
-
-
-
-
-
-}
-// looping through the game 5 times to play multiple rounds
-for (let i = 0; i <= 5; i++) {
-  game();
-}
